583d70f44a7604807d2fa36fccb5c4e034f60ae5
Mike King <mike@ikayzo.com>
2014-03-27 16:40:59 -1000

    Initial Commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('webappApp').controller('MainCtrl', function ($scope, $http, $interval) {
+
+	$scope.getPrice = function(){
+		$http.jsonp('https://coinbase.com/api/v1/prices/spot_rate?callback=JSON_CALLBACK').success(function(data){
+			$scope.data = data;
+		});
+	};
+
+	$scope.getPrice();
+
+	$interval($scope.getPrice, 60000);
+});
